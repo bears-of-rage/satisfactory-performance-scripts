74ba5ebcefaf2fecbacb2c574b4c53be13e118e5
pastaup <45907741+pasta-up@users.noreply.github.com>
2022-12-07 20:28:11 -0500

    fixed expression syntax

--- satisfactory-cron.sh
+++ satisfactory-cron.sh
@@ -4,7 +4,7 @@ GAME_RAMDRIVE_SAVES="${GAME_RD_LOC}/satisfactory-saves"
 GAME_RAMDRIVE_BINARIES="${GAME_RD_LOC}/satisfactory"
 WWW_ROOT="/var/www/html/"
 
-if [! -d $GAME_RD_LOC ]; then
+if [ ! -d $GAME_RD_LOC ]; then
   sudo mkdir -p $GAME_RD_LOC
   sudo mount -t tmpfs -o size=12288m satisfactory ${GAME_RD_LOC}
   rsync -a ${GAME_LOC}/satisfactory-saves ${GAME_RAMDRIVE_SAVES}
@@ -21,7 +21,7 @@ if [! -d $GAME_RD_LOC ]; then
   wait
   sleep 5
 else
-  if [! -d ${GAME_LOC}/satisfactory-saves]; then
+  if [ ! -d ${GAME_LOC}/satisfactory-saves]; then
     sudo mkdir -p ${GAME_LOC}/satisfactory-saves
   fi
   sudo rsync -a --delete ${GAME_RAMDRIVE_SAVES} ${GAME_LOC}/satisfactory-saves/
